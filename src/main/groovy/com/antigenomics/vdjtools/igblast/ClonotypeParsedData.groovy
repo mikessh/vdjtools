6757b793a3a454159a0c9b233f8ae9c4c725981b
mikesh <mikhail.shugay@gmail.com>
2014-06-11 21:27:59 +0400

    IgBlast parse infrastructure

---
+++ src/main/groovy/com/antigenomics/vdjtools/igblast/ClonotypeParsedData.groovy
@@ -0,0 +1,88 @@
+/**
+ Copyright 2014 Mikhail Shugay (mikhail.shugay@gmail.com)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package com.antigenomics.vdjtools.igblast
+
+class ClonotypeParsedData {
+
+    final Set mutations,
+              alleles = new HashSet<MutationParseData>(),
+              shms = new HashSet<MutationParseData>()
+
+    final int count
+    final double freq
+
+    final String v, d, j
+    final String cdr1nt, cdr2nt, cdr3nt, cdr1aa, cdr2aa, cdr3aa, key
+    final boolean inFrame, noStop, isComplete
+
+    /*
+    0	1	2	3
+    #reads_count	reads_percent	events_count	events_percent
+    4	5	6	7	8	9
+	cdr1nt	cdr2nt	cdr3nt	cdr1aa	cdr2aa	cdr3aa
+	10	11	12
+	inFrame	noStop	complete
+	13	14	15
+	vSegment	dSegment	jSegment
+	16	17	18
+	cdr1q	cdr2q	cdr3q
+	19
+	mutations
+     */
+
+    ClonotypeParsedData(String clonotypeString) {
+        def splitString = clonotypeString.split("\t")
+
+        count = splitString[2].toInteger()
+        freq = splitString[3].toDouble()
+
+        (cdr1nt, cdr2nt, cdr3nt, cdr1aa, cdr2aa, cdr3aa) = splitString[4..9]
+        (inFrame, noStop, isComplete) = splitString[10..12].collect { it.toBoolean() }
+        (v, d, j) = splitString[13..15]
+
+        mutations = splitString[19] == "." ? new HashSet<MutationParseData>() :
+                new HashSet<MutationParseData>(splitString[19].split("\\|").collect { new MutationParseData(it) })
+
+        key = [cdr3nt, mutations.collect { MutationParseData mpd -> "$mpd.ntPos:$mpd.fromNT>$mpd.toNT" }.join("|")].join("_")
+    }
+
+    //
+    // Display
+    //
+
+    String getDisplayName() {
+        "V" + v[2] + v[4..-4] + ":" + cdr3aa + ":S" + shms.size()
+    }
+
+    final static NODE_HEADER =
+            "display_name\tcount\tfreq\t" +
+                    "cdr1nt\tcdr2nt\tcdr3nt\t" +
+                    "cdr1aa\tcdr2aa\tcdr3aa\t" +
+                    "inFrame\tnoStop\tisComplete\t" +
+                    "V\tD\tJ\t" +
+                    "shms\talleles"
+
+    @Override
+    String toString() {
+        [displayName, count, freq,
+         cdr1nt, cdr2nt, cdr3nt,
+         cdr1aa, cdr2aa, cdr3aa,
+         inFrame, noStop, isComplete,
+         v, d, j,
+         shms.collect { it.key }.join("|"), alleles.collect { it.key }.join("|")].join("\t")
+    }
+}
---
+++ src/main/groovy/com/antigenomics/vdjtools/igblast/MutationParseData.groovy
@@ -0,0 +1,82 @@
+package com.antigenomics.vdjtools.igblast
+
+/**
+ Copyright 2014 Mikhail Shugay (mikhail.shugay@gmail.com)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+class MutationParseData {
+    final String key, aaString, region
+    final ntPos, aaPos
+    final char fromAA, toAA, fromNT, toNT
+    final boolean isSilent
+    final int nReads, nEvents
+    final double fReads, fEvents
+
+    MutationParseData(String mutationString) {
+        def splitString = mutationString.split(",")
+
+        def splitCountString = splitString[0].split(":")
+        nReads = splitCountString[0].toInteger()
+        fReads = splitCountString[1].toDouble()
+        nEvents = splitCountString[2].toInteger()
+        fEvents = splitCountString[3].toDouble()
+
+        key = splitString[1]
+        def splitNTString = key.split("[:>]")
+        ntPos = splitNTString[0].toInteger()
+        fromNT = splitNTString[1]
+        toNT = splitNTString[2]
+
+        aaString = splitString[2]
+        def splitAAString = aaString.split("[:>]")
+        aaPos = splitAAString[0].toInteger()
+        fromAA = splitAAString[1]
+        toAA = splitAAString[2]
+        isSilent = fromAA == toAA
+
+        region = splitString[3]
+    }
+
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (getClass() != o.class) return false
+
+        MutationParseData that = (MutationParseData) o
+
+        if (key != that.key) return false
+
+        return true
+    }
+
+    int hashCode() {
+        return key.hashCode()
+    }
+
+    //
+    // Display
+    //
+
+    final static String EDGE_HEADER = "display_name\t" +
+            "nt_shm\taa_shm\tregion\tsilent"
+
+    String getDisplayName() {
+        isSilent ? "S" : (region + ":" + fromAA + ">" + toAA)
+    }
+
+    @Override
+    String toString() {
+        [displayName, key, aaString, region, isSilent].join("\t")
+    }
+}
