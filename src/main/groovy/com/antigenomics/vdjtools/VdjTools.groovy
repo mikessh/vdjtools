3a6c0cc26524c9341015f7a4f2f3605b05140dc7
mikesh <mikhail.shugay@gmail.com>
2014-09-04 20:59:49 +0400

    Jar-based running mode implemented

---
+++ src/main/groovy/com/antigenomics/vdjtools/VdjTools.groovy
@@ -0,0 +1,81 @@
+package com.antigenomics.vdjtools
+
+import com.antigenomics.vdjtools.basic.CalcBasicStats
+import com.antigenomics.vdjtools.util.ExecUtil
+
+/**
+ Copyright 2014 Mikhail Shugay (mikhail.shugay@gmail.com)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import java.util.jar.JarFile
+
+def version = (getClass().classLoader.findResource(JarFile.MANIFEST_NAME).text =~
+        /Implementation-Version: (.+)/)[0][1]
+
+def printHelp = {
+    println " VdjTools V$version "
+    println ""
+    println "Run as \$java -jar vdjtools-${version}.jar SCRIPT_NAME arguments"
+    println ""
+    println "[Single-sample statistics]"
+    println ""
+    println "CalcBasicStats"
+}
+
+def getScript = { String scriptName ->
+    switch (scriptName.toUpperCase()) {
+        case "CALCBASICSTATS":
+            return new CalcBasicStats()
+        case "-H":
+        case "H":
+        case "-HELP":
+        case "HELP":
+            printHelp()
+            println ""
+            System.exit(-1)
+            break
+
+        default:
+            printHelp()
+            println ""
+            println "Unknown MAIN PIPELINE script $scriptName"
+            System.exit(-1)
+    }
+}
+
+if (args.length == 0)
+    printHelp()
+else {
+    def script = getScript(args[0])
+    try {
+        ExecUtil.run(script, args.length > 1 ? args[1..-1].join(" ") : "")
+    } catch (Exception e) {
+        println "[ERROR] $e.message, see _vdjtools_error.log for details"
+        new File("_vdjtools_error.log").withWriterAppend { writer ->
+            writer.println("[${new Date()}]")
+            writer.println("[Script]")
+            writer.println(args[0])
+            writer.println("[CommandLine]")
+            writer.println("executing vdjtools-${version}.jar ${args.join(" ")}")
+            writer.println("[Message]")
+            writer.println(e.message)
+            writer.println("[StackTrace-Short]")
+            writer.println(e.stackTrace.findAll { it.toString().contains("com.antigenomics.vdjtools") }.join("\n"))
+            writer.println("[StackTrace-Full]")
+            e.printStackTrace(new PrintWriter(writer))
+        }
+        System.exit(-1)
+    }
+}
