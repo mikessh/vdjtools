c3a01a71bec698d0582b89878a7ae13bb2c70ac9
mikesh <mikhail.shugay@gmail.com>
2014-09-09 17:38:39 +0400

    Cli for down-sampling

---
+++ src/main/groovy/com/antigenomics/vdjtools/diversity/DownSample.groovy
@@ -0,0 +1,99 @@
+/**
+ Copyright 2014 Mikhail Shugay (mikhail.shugay@gmail.com)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+
+package com.antigenomics.vdjtools.diversity
+
+import com.antigenomics.vdjtools.Software
+import com.antigenomics.vdjtools.sample.SampleCollection
+import com.antigenomics.vdjtools.util.ExecUtil
+
+def cli = new CliBuilder(usage: "DownSample [options] " +
+        "[sample1 sample2 sample3 ... if -m is not specified] output_prefix")
+cli.h("display help message")
+cli.S(longOpt: "software", argName: "string", required: true, args: 1,
+        "Software used to process RepSeq data. Currently supported: ${Software.values().join(", ")}")
+cli.m(longOpt: "metadata", argName: "filename", args: 1,
+        "Metadata file. First and second columns should contain file name and sample id. " +
+                "Header is mandatory and will be used to assign column names for metadata." +
+                "If column named 'time' is present, it will be used to specify time point sequence.")
+cli.n(longOpt: "num-cells", argName: "integer", required: true, args: 1, "Number of reads/cDNAs to sample.")
+
+def opt = cli.parse(args)
+
+if (opt == null)
+    System.exit(-1)
+
+if (opt.h || opt.arguments().size() == 0) {
+    cli.usage()
+    System.exit(-1)
+}
+
+// Check if metadata is provided
+
+def metadataFileName = opt.m
+
+if (metadataFileName ? opt.arguments().size() != 1 : opt.arguments().size() < 2) {
+    if (metadataFileName)
+        println "Only output prefix should be provided in case of -m"
+    else
+        println "At least 1 sample files should be provided if not using -m"
+    cli.usage()
+    System.exit(-1)
+}
+
+// Remaining arguments
+
+def software = Software.byName(opt.S), n = opt.n.toInteger(),
+    outputPrefix = opt.arguments()[-1]
+
+ExecUtil.ensureDir(outputPrefix)
+
+def scriptName = getClass().canonicalName.split("\\.")[-1]
+
+//
+// Batch load all samples (lazy)
+//
+
+println "[${new Date()} $scriptName] Reading sample(s)"
+
+def sampleCollection = metadataFileName ?
+        new SampleCollection((String) metadataFileName, software, false, true) :
+        new SampleCollection(opt.arguments()[0..-2], software, false)
+
+println "[${new Date()} $scriptName] ${sampleCollection.size()} sample(s) loaded"
+
+//
+// Iterate over samples & down-sample
+//
+
+sampleCollection.eachWithIndex { sample, ind ->
+    def downSampler = new DownSampler(sample)
+    def newSample = downSampler.reSample(n)
+
+    // recompute frequencies and sort
+    newSample.renormalize()
+    newSample.sort()
+
+    println "[${new Date()} $scriptName] Processed ${ind + 1} sample(s).."
+
+    // print output
+    new File(outputPrefix + "." + sample.sampleMetadata.sampleId + ".txt").withPrintWriter { pw ->
+        newSample.print(pw, software, true)
+    }
+}
+
+println "[${new Date()} $scriptName] Finished"
