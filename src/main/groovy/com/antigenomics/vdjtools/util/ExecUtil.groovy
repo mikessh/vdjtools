27f8aa4c80adf891b74e41e3d8fa8436272b0b0e
mikesh <mikhail.shugay@gmail.com>
2014-11-15 07:50:43 +0300

    Utils for I/O logistics, on the road to issue #6

--- src/main/groovy/com/antigenomics/vdjtools/util/ExecUtil.groovy
+++ src/main/groovy/com/antigenomics/vdjtools/util/ExecUtil.groovy
@@ -1,26 +1,35 @@
-/**
- Copyright 2014 Mikhail Shugay (mikhail.shugay@gmail.com)
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+/*
+ * Copyright 2013-2014 Mikhail Shugay (mikhail.shugay@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Last modified on 15.11.2014 by mikesh
  */
+
+
 
 package com.antigenomics.vdjtools.util
 
+import java.nio.file.FileSystems
+import java.nio.file.Path
+
 
 class ExecUtil {
-    static final int THREADS = Runtime.runtime.availableProcessors()
+    // todo: use this everywhere
 
-    static String memoryFootprint() {
+    public static final int THREADS = Runtime.runtime.availableProcessors()
+
+    public static String memoryFootprint() {
         final factor = 1024 * 1024 * 1024
 
         int maxMemory = Runtime.runtime.maxMemory() / factor,
@@ -30,18 +39,18 @@ class ExecUtil {
         "Memory usage: $allocatedMemory of ${maxMemory + freeMemory} GB"
     }
 
-    static void report(Object me, String message) { // todo: use this everywhere
+    public static void report(Object me, String message) {
         report(me, message, true)
     }
 
-    static void report(Object me, String message, boolean verbose) { // todo: use this everywhere
+    public static void report(Object me, String message, boolean verbose) {
         if (verbose) {
             def scriptName = me.class.canonicalName.split("\\.")[-1]
             println "[${new Date()} $scriptName] $message"
         }
     }
 
-    static Object run(Script script, String args) {
+    public static Object run(Script script, String args) {
         // perform cleanup
         def argArray = args.split(" ").
                 findAll { it != " " && it != "" }.
@@ -51,18 +60,81 @@ class ExecUtil {
         script.run()
     }
 
-    static void ensureDir(String fileName) {
-        new File(fileName).absoluteFile.parentFile.mkdirs()
+    /**
+     * Routines for I/O logistics
+     */
+
+    /**
+     * Makes sure that a given path exists
+     * @param path
+     */
+    public static void ensureDir(String path) {
+        new File(path).absoluteFile.parentFile.mkdirs()
     }
 
-    static String createTempDir(String outputPrefix) {
-        def tmpPath = new File(outputPrefix).absolutePath + "-vdjtools-" + UUID.randomUUID().toString()
+    /**
+     * Creates a temporary directory
+     * @param pathPrefix
+     * @return
+     */
+    public static String createTempDir(String pathPrefix) {
+        def tmpPath = getAbsolutePath(pathPrefix).toString() + "-vdjtools-" + UUID.randomUUID().toString()
 
         def tmpFolderFile = new File(tmpPath)
         tmpFolderFile.mkdirs()
 
-        //tmpFolderFile.deleteOnExit()
+        tmpFolderFile.deleteOnExit()
 
         tmpPath
+    }
+
+    /**
+     * Internal
+     */
+    private static Path getAbsolutePath(String path) {
+        getPath(path).toAbsolutePath()
+    }
+
+    /**
+     * Internal
+     */
+    private static Path getPath(String path) {
+        FileSystems.default.getPath(path)
+    }
+
+    /**
+     * Provides a relative path to a sample given metadata path as reference.
+     * Mainly used to store sample path in newly created metadata files
+     * @param metadataPath path to a metadata file
+     * @param samplePath path to a sample file
+     * @return
+     */
+    public static String relativeSamplePath(String metadataPath, String samplePath) {
+        getAbsolutePath(metadataPath).relativize(getAbsolutePath(samplePath)).toString()
+    }
+
+    /**
+     * Gets the absolute path to a sample file using metadata path as reference
+     * @param metadataPath path to a metadata file
+     * @param samplePath path to a sample file as recorded in metadata file
+     * @return
+     */
+    public static String absoluteSamplePath(String metadataPath, String samplePath) {
+        if (new File(samplePath).isAbsolute()) {
+            return samplePath
+        }
+        getAbsolutePath(metadataPath).parent.resolve(samplePath).normalize().toAbsolutePath().toString()
+    }
+
+    /**
+     * Gets the output path according to VDJtools convention, with file name parts joined by '.'
+     * @param outputPrefix output prefix, either directory name or directory name + prefix
+     * @param outputSuffix one or more output suffices
+     * @return output/prefix.suffix1.suffix2 or outputPrefix/suffix1.suffix2
+     */
+    public static String formOutputPath(String outputPrefix,
+                                    String... outputSuffix) {
+        def s = outputSuffix.join(".")
+        outputPrefix.endsWith(File.pathSeparator) ? (outputPrefix + s) : (outputPrefix + "." + s)
     }
 }
