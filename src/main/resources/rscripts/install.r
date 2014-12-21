d3e665663abc512fa490d5c11efc0ee84c261f8e
mikesh <mikhail.shugay@gmail.com>
2014-12-22 02:17:15 +0300

    2prev

--- src/main/resources/rscripts/install.r
+++ src/main/resources/rscripts/install.r
@@ -5,7 +5,6 @@ libPath <- args[1]
 # iterate over dependencies
 for (i in 2:length(args)) {
    # install packages to local library, use 0-Cloud mirror
-   # note that local library is automatically included
-   # via `R_LIBS` environmental variable set by RUtil
+   # note that local library is automatically to search path by RUtil
    install.packages(args[i], lib=libPath, repos="http://cran.rstudio.com/")
 }
