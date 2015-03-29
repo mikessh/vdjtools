f8b3f7a3a14a6a69c993104a76c0f62c1dde6240
mikesh <mikhail.shugay@gmail.com>
2015-03-29 21:58:30 +0300

    Add Venn plot coloring

--- src/main/resources/rscripts/join_venn.r
+++ src/main/resources/rscripts/join_venn.r
@@ -49,25 +49,25 @@ out_file <- args[33]
 if (is.na(area3)) {
   venn.plot <- draw.pairwise.venn(area1, area2,
                                   n12,
-                                  category[1:2])
+                                  category[1:2], fill=brewer.pal(2, "Paired"), alpha=rep(0.5,2))
 } else if (is.na(area4)) {
   venn.plot <- draw.triple.venn(area1, area2, area3,
                                 n12, n23, n13,
                                 n123,
-                                category[1:3])
+                                category[1:3], fill=brewer.pal(3, "Paired"), alpha=rep(0.5,3))
 } else if (is.na(area5)) {
   venn.plot <- draw.quad.venn(area1, area2, area3, area4,
                               n12, n13, n14, n23, n24, n34,
                               n123, n124,
                               n134, n234, n1234,
-                              category[1:4])
+                              category[1:4], fill=brewer.pal(4, "Paired"), alpha=rep(0.5,4))
 } else {
   venn.plot <- draw.quintuple.venn(area1, area2, area3, area4, area5,
                                    n12, n13, n14, n15, n23, n24, n25,
                                    n34, n35, n45, n123, n124, n125, n134,
                                    n135, n145, n234, n235, n245, n345,
                                    n1234, n1235, n1245, n1345, n2345, n12345,
-                                   category[1:5])
+                                   category[1:5], fill=brewer.pal(5, "Paired"), alpha=rep(0.5,5))
 }
 
 pdf(out_file)
