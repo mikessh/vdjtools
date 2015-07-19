8c10d3f061e917cce069eb7493011ce120e69f86
mikesh <mikhail.shugay@gmail.com>
2015-07-19 21:11:43 +0200

    Fix bug in venn diagram plotting

--- src/main/resources/rscripts/join_venn.r
+++ src/main/resources/rscripts/join_venn.r
@@ -8,40 +8,69 @@ area3    <- as.integer(args[3])
 area4    <- as.integer(args[4])
 area5    <- as.integer(args[5])
 
+n1234    <- as.integer(args[26])
+n1235    <- as.integer(args[27])
+n1245    <- as.integer(args[28])
+n1345    <- as.integer(args[29])
+n2345    <- as.integer(args[30])
+
+n123     <- as.integer(args[16])
+n124     <- as.integer(args[17])
+n125     <- as.integer(args[18])
+n134     <- as.integer(args[19])
+n135     <- as.integer(args[20])
+n145     <- as.integer(args[21])
+n234     <- as.integer(args[22])
+n235     <- as.integer(args[23])
+n245     <- as.integer(args[24])
+n345     <- as.integer(args[25])
+
+n12      <- as.integer(args[6])
+n13      <- as.integer(args[7])
+n14      <- as.integer(args[8])
+n15      <- as.integer(args[9])
+n23      <- as.integer(args[10])
+n24      <- as.integer(args[11])
+n25      <- as.integer(args[12])
+n34      <- as.integer(args[13])
+n35      <- as.integer(args[14])
+n45      <- as.integer(args[15])
+
 n12345   <- as.integer(args[31])
 
+# cumulative
 # yes we have to integrate so that the VennDiagram will recalculate back
 # no option to provide raw counts unfortunately
 # haven't found any other package that accepts collapsed counts
 # (imagine writing to file and loading 20mln binary strings to R)
 
-n1234    <- as.integer(args[26])+n12345
-n1235    <- as.integer(args[27])+n12345
-n1245    <- as.integer(args[28])+n12345
-n1345    <- as.integer(args[29])+n12345
-n2345    <- as.integer(args[30])+n12345
+n12      <- n12+n123+n124+n125+n1234+n1235+n1245+n12345
+n13      <- n13+n123+n134+n135+n1234+n1235+n1345+n12345
+n14      <- n14+n124+n134+n145+n1234+n1245+n1345+n12345
+n15      <- n15+n125+n135+n145+n1235+n1245+n1345+n12345
+n23      <- n23+n123+n234+n235+n1234+n1235+n2345+n12345
+n24      <- n24+n124+n234+n245+n1234+n1245+n2345+n12345
+n25      <- n25+n125+n235+n245+n1235+n1245+n2345+n12345
+n34      <- n34+n134+n234+n345+n1234+n1345+n2345+n12345
+n35      <- n35+n135+n235+n345+n1235+n1345+n2345+n12345
+n45      <- n45+n145+n245+n345+n1245+n1345+n2345+n12345
 
-n123     <- as.integer(args[16])+n1234+n1235
-n124     <- as.integer(args[17])+n1234+n1245
-n125     <- as.integer(args[18])+n1235+n1245
-n134     <- as.integer(args[19])+n1234+n1345
-n135     <- as.integer(args[20])+n1235+n1345
-n145     <- as.integer(args[21])+n1245+n1345
-n234     <- as.integer(args[22])+n1234+n2345
-n235     <- as.integer(args[23])+n1235+n2345
-n245     <- as.integer(args[24])+n1245+n2345
-n345     <- as.integer(args[25])+n1345+n2345
+n123     <- n123+n1234+n1235+n12345
+n124     <- n124+n1234+n1245+n12345
+n125     <- n125+n1235+n1245+n12345
+n134     <- n134+n1234+n1345+n12345
+n135     <- n135+n1235+n1345+n12345
+n145     <- n145+n1245+n1345+n12345
+n234     <- n234+n1234+n2345+n12345
+n235     <- n235+n1235+n2345+n12345
+n245     <- n245+n1245+n2345+n12345
+n345     <- n345+n1345+n2345+n12345
 
-n12      <- as.integer(args[6])+n123+n124+n125
-n13      <- as.integer(args[7])+n123+n134+n135
-n14      <- as.integer(args[8])+n124+n134+n145
-n15      <- as.integer(args[9])+n125+n135+n145
-n23      <- as.integer(args[10])+n123+n234+n235
-n24      <- as.integer(args[11])+n124+n234+n245
-n25      <- as.integer(args[12])+n125+n235+n245
-n34      <- as.integer(args[13])+n134+n234+n345
-n35      <- as.integer(args[14])+n135+n235+n345
-n45      <- as.integer(args[15])+n145+n245+n345
+n1234    <- n1234+n12345
+n1235    <- n1235+n12345
+n1245    <- n1245+n12345
+n1345    <- n1345+n12345
+n2345    <- n2345+n12345
 
 category <- strsplit(args[32],split=",")[[1]]
 file_out <- args[33]
