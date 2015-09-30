77f048fe0dc2f98462128f9af8687df480bf3e79
mikesh <mikhail.shugay@gmail.com>
2015-09-30 15:16:27 +0300

    #29: select top clonotypes

---
+++ src/main/groovy/com/antigenomics/vdjtools/preprocess/SelectTop.groovy
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2015, Bolotin Dmitry, Chudakov Dmitry, Shugay Mikhail
+ * (here and after addressed as Inventors)
+ * All Rights Reserved
+ *
+ * Permission to use, copy, modify and distribute any part of this program for
+ * educational, research and non-profit purposes, by non-profit institutions
+ * only, without fee, and without a written agreement is hereby granted,
+ * provided that the above copyright notice, this paragraph and the following
+ * three paragraphs appear in all copies.
+ *
+ * Those desiring to incorporate this work into commercial products or use for
+ * commercial purposes should contact the Inventors using one of the following
+ * email addresses: chudakovdm@mail.ru, chudakovdm@gmail.com
+ *
+ * IN NO EVENT SHALL THE INVENTORS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+ * SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+ * ARISING OUT OF THE USE OF THIS SOFTWARE, EVEN IF THE INVENTORS HAS BEEN
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THE SOFTWARE PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND THE INVENTORS HAS
+ * NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
+ * MODIFICATIONS. THE INVENTORS MAKES NO REPRESENTATIONS AND EXTENDS NO
+ * WARRANTIES OF ANY KIND, EITHER IMPLIED OR EXPRESS, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A
+ * PARTICULAR PURPOSE, OR THAT THE USE OF THE SOFTWARE WILL NOT INFRINGE ANY
+ * PATENT, TRADEMARK OR OTHER RIGHTS.
+ */
+
+package com.antigenomics.vdjtools.preprocess
+
+import com.antigenomics.vdjtools.io.SampleWriter
+import com.antigenomics.vdjtools.sample.BlankClonotypeFilter
+import com.antigenomics.vdjtools.sample.Sample
+import com.antigenomics.vdjtools.sample.SampleCollection
+
+
+def cli = new CliBuilder(usage: "SelectTop [options] " +
+        "[sample1 sample2 sample3 ... if -m is not specified] output_prefix")
+cli.h("display help message")
+cli.m(longOpt: "metadata", argName: "filename", args: 1,
+        "Metadata file. First and second columns should contain file name and sample id. " +
+                "Header is mandatory and will be used to assign column names for metadata.")
+cli.x(longOpt: "num-clonotypes", argName: "integer", required: true, args: 1, "Number of top clonotypes to take.")
+cli.c(longOpt: "compress", "Compress output sample files.")
+
+def opt = cli.parse(args)
+
+if (opt == null)
+    System.exit(-1)
+
+if (opt.h || opt.arguments().size() == 0) {
+    cli.usage()
+    System.exit(-1)
+}
+
+// Check if metadata is provided
+
+def metadataFileName = opt.m
+
+if (metadataFileName ? opt.arguments().size() != 1 : opt.arguments().size() < 2) {
+    if (metadataFileName)
+        println "Only output prefix should be provided in case of -m"
+    else
+        println "At least 1 sample files should be provided if not using -m"
+    cli.usage()
+    System.exit(-1)
+}
+
+// Remaining arguments
+
+def x = (int) opt.x.toInteger(),
+    compress = (boolean) opt.c,
+    outputPrefix = opt.arguments()[-1]
+
+def scriptName = getClass().canonicalName.split("\\.")[-1]
+
+//
+// Batch load all samples (lazy)
+//
+
+println "[${new Date()} $scriptName] Reading sample(s)"
+
+def sampleCollection = metadataFileName ?
+        new SampleCollection((String) metadataFileName) :
+        new SampleCollection(opt.arguments()[0..-2])
+
+println "[${new Date()} $scriptName] ${sampleCollection.size()} sample(s) loaded"
+
+//
+// Iterate over samples & down-sample
+//
+
+def sampleWriter = new SampleWriter(compress)
+
+sampleCollection.eachWithIndex { sample, ind ->
+    def newSample = new Sample(sample, BlankClonotypeFilter.INSTANCE, x)
+
+    println "[${new Date()} $scriptName] Processed ${ind + 1} sample(s).. Writing output"
+
+    // print output
+    sampleWriter.writeConventional(newSample, outputPrefix)
+}
+
+sampleCollection.metadataTable.storeWithOutput(outputPrefix, compress, "top:$x")
+
+println "[${new Date()} $scriptName] Finished"
