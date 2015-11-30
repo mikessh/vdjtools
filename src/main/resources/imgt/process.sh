530e7d86c6676bbab115726131f795a265aa9f83
mikessh <mikhail.shugay@gmail.com>
2015-11-30 20:36:37 +0300

    More cleanup

--- src/main/resources/imgt/process.sh
+++
@@ -1,5 +0,0 @@
-IMGTPARSER="java -cp ../../../../target/vdjtools-1.0-SNAPSHOT.jar com.antigenomics.vdjtools.imgt.ImgtParser"
-$IMGTPARSER -b F+ORF+in-frame_P_gaps_05-06-14.imgt segments
-$IMGTPARSER -nb F+ORF+in-frame_P_gaps_05-06-14.imgt segments.all
-$IMGTPARSER -nmb F+ORF+in-frame_P_gaps_05-06-14.imgt segments.all.minor
-$IMGTPARSER -mb F+ORF+in-frame_P_gaps_05-06-14.imgt segments.minor
