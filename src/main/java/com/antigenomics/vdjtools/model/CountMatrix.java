1b5bdbf27b50acfd5d4bc96421ad0f7017337495
mikessh <mikhail.shugay@gmail.com>
2017-07-18 20:02:30 +0300

    2prev

--- src/main/java/com/antigenomics/vdjtools/model/CountMatrix.java
+++
@@ -1,9 +0,0 @@
-package com.antigenomics.vdjtools.model;
-
-import com.antigenomics.vdjtools.sample.Clonotype;
-
-public interface CountMatrix {
-    void update(Clonotype clonotype);
-
-    long getCount(Clonotype clonotype);
-}
